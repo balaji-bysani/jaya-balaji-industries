c0eef245c116fad5e248f042bb674c2dcaeb43b8
Devi Sri <pallothudevisri@gmail.com>
2025-03-04 13:59:47 +0530

    Adding login and register

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,48 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
+import {Login} from "./components/login"
+import {Register} from "./components/Register"
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [userstate, setUserState] = useState({});
   return (
-    <div>
-      <h1>JBI</h1>
-      <Button>Click me!</Button>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
+      <BrowserRouter>
+        <div>
+          
+          
+            <div>
+             
+              <Routes>
+                {/* dashboard  */}
+                <Route
+                  path="/"
+                  element={
+                    userstate && userstate._id ? (
+                      <Profile
+                        setUserState={setUserState}
+                        username={userstate.fname}
+                      />
+                    ) : (
+                      <Login setUserState={setUserState} />
+                    )
+                  }
+                ></Route>
+                <Route
+                  path="/login"
+                  element={<Login setUserState={setUserState} />}
+                ></Route>
+                <Route path="/signup" element={<Register />} />
+
+
+              </Routes>
+            </div>
+          
+          </div>
+        
+      </BrowserRouter>
     </div>
   )
 }
